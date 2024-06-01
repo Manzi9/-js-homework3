2f0e5274f58887081dc6ad30ff03808bdfc6dbc5
Your Name <you@example.com>
2024-06-01 16:56:40 +0100

    test

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 
 function func1() {
   console.log("Function1");
-  function func2() {
-    console.log("Function2");
-    function func3() {
-      console.log("Function3");
-    }
-  }
+  //   function func2() {
+  //     console.log("Function2");
+  //     function func3() {
+  //       console.log("Function3");
+  //     }
+  //   }
 }
-//not working
+
+func1();
